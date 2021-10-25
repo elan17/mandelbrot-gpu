0deed2cfe149149a5a14cafecc2299caacebd451
Juan Toca <elan17.programacion@gmail.com>
2021-10-25 23:42:42 +0200

    Created abstract class for the backends

---
+++ backends/backend.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+class Backend:
+
+    @abstractmethod
+    def __init__(self, initial_array: np.ndarray):
+        pass
+
+    @abstractmethod
+    def update(self):
+        pass
+
+    @abstractmethod
+    def move(self, vector: tuple):
+        pass
+
+    @abstractmethod
+    def zoom(self, ratio: float):
+        pass
